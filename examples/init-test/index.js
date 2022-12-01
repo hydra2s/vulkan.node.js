309527fa5469bcef4e4cffb0c913ff42e671f08c
unit-a-user <helixd.2s18@gmail.com>
2022-12-01 14:18:39 +0800

    npm run test

---
+++ examples/init-test/index.js
@@ -0,0 +1,8 @@
+(async()=>{
+
+    //
+    const V = (await import("../../index.js")).default;
+    const L = (await import("../library/index.js")).default;
+    const fs = await import("fs");
+
+})();
